b785f0d8dfc303154f890e585c9f3512fc62baf7
Asger Askov Blekinge <abr@kb.dk>
2021-06-04 13:43:02 +0200

    Include size of produced index

--- mediestream/finishedStatus.sh
+++ mediestream/finishedStatus.sh
@@ -1,10 +1,15 @@
 SCRIPT_DIR=$(dirname "$(readlink -f -- ${BASH_SOURCE[0]})")
 
-echo "logfile,indexing Time,mp3 count,mp3 total size"
+echo -e "logfile\t indexing Time(seconds)\t mp3 count\t mp3 size(megabytes)\t index size(megabytes)"
 grep -l "Finished" "$SCRIPT_DIR/"*.log | while read -r file; do
     grep -q '^List consist of\s\+[0-9]\+' "$file" || continue;
     count=$(grep '^List consist of' "$file" | cut -d' ' -f4);
     duration=$(grep 'for list' "$file" | cut -d' ' -f8- | sed 's/maj/may/' |xargs -r -I'{2}' date -d'{2}' +%s | awk 'NR%2{printf "-%s+",$0;next;}1' | bc -q);
-    mp3Size=$(cat "/data01/larm/mediestream-index/ismirInput/$(basename $file .log)" | tr '\n' '\0' | du -ch --files0-from=- | grep 'total'| cut -f1);
-    echo "$(basename $file),$duration seconds,$count files,$mp3Size";
+    mp3Size=$(cat "/data01/larm/mediestream-index/ismirInput/$(basename $file .log)" | tr '\n' '\0' | du -chm --files0-from=- | grep 'total'| cut -f1);
+    indexSize=$(du -chm "/data01/larm/mediestream-index/index/$(basename $file .log).index" | grep 'total'| cut -f1);
+    echo -e "$(basename $file)\t$duration\t$count\t$mp3Size\t$indexSize";
 done
+
+# Indexing time on miaplacidus seems to follow the formula
+# Time_in_seconds = 0.66 * Size_Megabytes
+
